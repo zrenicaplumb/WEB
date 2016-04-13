4b5b9ab8aa4105eba2a8047580c469f88408c689
zrenicaplumb <zrenicaplumb@gmail.com>
2016-04-12 18:40:00 -0600

    new stuff

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -15,5 +15,34 @@ $(document).ready(function(){
 	});
 
 
-})
+	 $("*").find("a[href='"+window.location.href+"']").each(function(){
+        $(this).addClass("active");
+       
+    })
+    
+function checkName(){
+	var form = $('form');
+   var nameWarning = $("<span>Please enter your full name</span>");
+   var input = document.getElementsByTagName('form')[0].getElementsByTagName('input')[0].value;
+   if(input.length < 2 || isNaN(input) == false){ 
+   console.log('u suck');
+   
+   nameWarning.addClass('nameWarning');
+   
+   form.prepend(nameWarning);
+   
+   
+   document.getElementsByTagName('form')[0].getElementsByTagName('input')[0].className += ' error';
+}     
+else{     
+	console.log(nameWarning);
+	$(document).detach(nameWarning);
+	
+	document.getElementsByTagName('form')[0].getElementsByTagName('input')[0].className = document.getElementsByTagName('form')[0].getElementsByTagName('input')[0].className.replace('error','')    } }
+
+
+   document.getElementsByTagName('form')[0].getElementsByTagName('input')[0].onblur = checkName;
+
+
+});
 	
