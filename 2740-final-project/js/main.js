664a37a7b6491a336917a200c0538987b6cc3040
zrenicaplumb <zrenicaplumb@gmail.com>
2016-04-16 14:45:57 -0600

    unfinished project

---
+++ 2740-final-project/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	function initialize() {
+		  var mapProp = {
+		    center:new google.maps.LatLng(38.572363,-109.540917),
+		    zoom:18,
+		    mapTypeId:google.maps.MapTypeId.ROADMAP
+		  };
+		  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+		}
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
+
+});
