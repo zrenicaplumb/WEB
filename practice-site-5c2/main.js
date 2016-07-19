497349fed6cf551211f80ea8cc19e8a43004e80f
Kosmic Kitty <zrenicaplumb@gmail.com>
2016-07-19 12:02:17 -0600

    .
    .

---
+++ practice-site-5c2/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	var mobileMenu = $(".mobile-menu");
+	var sidebar = $(".sidebar");
+
+	mobileMenu.on("click",function(){
+		sidebar.addClass("open");
+
+	})
+	
+	
+})
+		
